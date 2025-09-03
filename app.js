1043d07a9745d5d53b95dc2de8597a7560d77979
Brian Crowe <brian.j.crowe@protonmail.com>
2025-09-02 21:31:29 -0400

    error handling todos 1, 2, and 3 complete

--- app.js
+++ app.js
@@ -1,48 +1,47 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
 /* Import route definitions */
-const quotesRouter = require('./routes/quotes');
+const quotesRouter = require('./routes/quotes')
 
 /* Instantiate Express app */
-const app = express();
+const app = express()
 
 /* Setup view engine */
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
 /* Express middleware for accessing the req.body */
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 /* Static middleware for serving static files*/
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 /* Root route redirect to the '/quotes' route */
 app.get('/', (req, res) => {
-  res.redirect('/quotes');
-});
+  res.redirect('/quotes')
+})
 
 /* Use route definitions */
-app.use('/quotes', quotesRouter);
-
+app.use('/quotes', quotesRouter)
 
 /* ERROR HANDLERS */
-/* 404 handler to catch undefined or non-existent route requests */ 
+/* 404 handler to catch undefined or non-existent route requests */
 app.use((req, res, next) => {
-  console.log('404 error handler called');
+  console.log('404 error handler called')
 
   /* TODO 1: Send a response to the client
     - Set the response status to 404
     - Render the 'not-found' view
-  */ 
-});
+  */
+  res.status(404).render('not-found')
+})
 
 /* Global error handler */
 app.use((err, req, res, next) => {
-
   if (err) {
-    console.log('Global error handler called', err);
+    console.log('Global error handler called', err)
   }
 
   /* TODO 2: Handle errors caught by your route handlers
@@ -56,6 +55,14 @@ app.use((err, req, res, next) => {
           set it to 500 by default if no error status is set
         * Render the 'error' view, passing it the error object
   */
-});
+  if (err.status === 404) {
+    // 404 Error
+    res.status(404).render('not-found', { error: err })
+  } else {
+    // General / Server Error
+    err.message = err.message || 'Oops! Something went wrong on the server.'
+    res.status(err.status || 500).render('error', { error: err })
+  }
+})
 
-module.exports = app;
+module.exports = app
--- routes/quotes.js
+++ routes/quotes.js
@@ -1,31 +1,29 @@
-const express = require('express');
-const router = express.Router();
-const { quotes } = require('../data');
+const express = require('express')
+const router = express.Router()
+const { quotes } = require('../data')
 
 /* GET quotes - render index template, passing it a title prop and the list of quotes */
 router.get('/', (req, res, next) => {
-
   // Log out home route handler indication
-  console.log('Home route called');
+  console.log('Home route called')
 
-  res.render('index', { title: 'Code Quotes', quotes });
-});
+  res.render('index', { title: 'Code Quotes', quotes })
+})
 
 /* GET generated error route - create and throw 500 server error */
 router.get('/error', (req, res, next) => {
-
   // Log out custom error handler indication
-  console.log('Custom error route called');
+  console.log('Custom error route called')
 
-  const err = new Error();
+  const err = new Error()
   err.message = `Custom 500 error thrown`
-  err.status = 500;
-  throw err;
-});
+  err.status = 500
+  throw err
+})
 
 /* GET individual quote route */
 router.get('/:id', (req, res, next) => {
-  console.log(`Quote ${req.params.id} route called`);
+  console.log(`Quote ${req.params.id} route called`)
 
   /* TODO 3: Check if the requested quote exists 
       - If quote exists, render the 'quote' view with the quote
@@ -34,8 +32,17 @@ router.get('/:id', (req, res, next) => {
         * Provide an error message
         * Forward the error to the global error handler
   */
+  const quote = quotes[req.params.id] // Lookup the quote by id
 
-  res.render('quote', { title: 'Code Quote', quote: quotes[req.params.id] });
-});
+  if (quote) {
+    // If the quote exists, render the quote view
+    res.render('quote', { title: 'Code Quote', quote })
+  } else {
+    // If not, create a 404 error and forward it
+    const err = new Error('Quote not found')
+    err.status = 404
+    next(err)
+  }
+})
 
-module.exports = router;
+module.exports = router
